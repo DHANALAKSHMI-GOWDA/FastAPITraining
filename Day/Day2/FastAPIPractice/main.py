9a659ae57c09e6eedb3b328d82ee6073c5cda4b1
DHANALAKSHMI-GOWDA <dhanalakshmikr767@gmail.com>
2026-09-22 13:30:14 +0530

    Path Parameters with Type Hint

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -1,19 +1,23 @@
-from fastapi import FastAPI
-app = FastAPI()
+from fastapi import  FastAPI
+app=FastAPI()
 @app.get("/")
 def home():
-    return {"page":"Home"}
+    return{"page":"Home"} 
 @app.get("/about")
 def about():
-    return {"page":"About","author":"Dhanu"}
+    return{"page":"About","author":"Brunda"}
 @app.get("/health")
 def health():
     return{"status":"ok"}
 # POST request
 @app.post("/create")
 def create_something():
-    return {"message":"created"}
-#Path parameters
-@app.get("/student/{usn}")
-def get_result(usn):
-    return {"Result":"Distinction","usn":usn}
+    return {"message":"Created"}
+#Path Parameters
+@app.get("/student/{rollno}")
+def get_result(rollno):
+    return {"Result":"Distinction","rollno":rollno}
+#Path Parameters with Type Hint
+@app.get("/candidate/{rollno}")
+def get_candidate(rollno:int):
+    return {"Result":"Distinction","rollno":rollno,"type":str(type(rollno))}
